084b4b71cd337cfae96cf9e315c3449ac4939bdb
Zac Wellmer <wellmerz@msu.edu>
2015-04-19 12:27:37 +0200

    updated to account for A* and Recursive best first

--- Heureka.py
+++ Heureka.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 class Road:
@@ -15,10 +16,17 @@ class Road:
         self.x_finish=int(words[3])
         self.y_finish=int(words[4])
 
+class Route:
+    def __init__(self, loc, final):
+        self.path=[loc] #path taken
+        self.cost = distance(loc[0], loc[1], final[0], final[1])
 
-
-
-
+def calc_cost(loc1, loc2, destination):
+    h=distance(loc2[0], loc2[1], destination[0], destination[1]) #distance to loc from next pos
+    g=distance(loc1[0], loc1[1], loc2[0], loc2[1]) #estimated distance from next node to goal
+    old_dist= distance(loc1[0], loc1[1], destination[0], destination[1])
+    f= h + g - old_dist #subtract old dist because we would have every nodes distance to final in calculation
+    return f
 
 #returns array of all road objects
 def roads_array():
@@ -35,52 +43,86 @@ def distance(x1, y1, x2, y2):
     dist=math.sqrt((x2-x1)**2+(y2-y1)**2) # ** is exponent in python
     return dist
 
+def possible_nodes(roads, loc, final):
+    to_explore=np.zeros((1,2)) #list of points to explore
 
-
-roads = roads_array()
-loc_x = 10
-loc_y = 70
-final_x = 80
-final_y = 70
-"""
-print("x:"+str(loc_x) + " y:" +str(loc_y))
-print("x:"+str(roads[0].x_start) + " y:" +str(roads[0].y_start))
-print(type(loc_x))
-print(type(roads[0].x_start))
-
-if loc_x == roads[0].x_start and loc_y == roads[0].y_start:
-    print("yay")
-"""
-while loc_x != final_x or loc_y != final_y:
     for road in roads:
-        if ((loc_x == road.x_start and loc_y == road.y_start)
-        or (loc_x == road.x_finish and loc_y == road.y_finish)):
+
+        if ((loc[0] == road.x_start and loc[1] == road.y_start) #checks for possible points
+        or (loc[0] == road.x_finish and loc[1] == road.y_finish)):
+
             possible_xloc = road.x_start
             possible_yloc = road.y_start
 
+
             #makes sure next point is not same as current point
-            if possible_xloc == loc_x and possible_yloc == loc_y:
+            if possible_xloc == loc[0] and possible_yloc == loc[1]:
                 possible_xloc = road.x_finish
                 possible_yloc = road.y_finish
 
-
+            possible = [possible_xloc, possible_yloc]
             #consider changing to less than or equal to
-            dist1 = distance(loc_x, loc_y, final_x, final_y)
-            dist2 = distance(possible_xloc, possible_yloc, final_x, final_y)
-            print("dist1:" + str(dist1) + " dist2:" + str(dist2))
+            #BE CAREFUL THIS IS WHERE COULD GET STUCK AT BOTTOM OF HILL
+            dist1 = distance(loc[0], loc[1], final[0], final[1])
+            dist2 = distance(possible[0], possible[1], final[0], final[1])
+            #print("dist1:" + str(dist1) + " dist2:" + str(dist2))
             if dist1 < dist2:
                 continue
 
+            to_explore = np.vstack((to_explore, possible)) #appends to matrix of all possible points to explore
 
-            loc_x=possible_xloc
-            loc_y=possible_yloc
-            print("changed road")
-            print("x:"+str(loc_x) + " y:" +str(loc_y))
-            if(loc_x == final_x and loc_y== final_y):
-                break
+    return to_explore
+
+def path_finder(roads, loc, final):
+
+    routes_traveled = []
+    new_route = Route(loc, final)
+
+    #new_route.path.append(loc) #initializes starting position
+    routes_traveled.append(new_route)
+    while loc[0] != final[0] or loc[1] != final[1]: #while not at final pos
+
+        to_explore = possible_nodes(roads, loc, final) #points to explore
+
+        #THIS UPDATES ROUTES TRAVELS WITHOUT ADDING NEW ROW
+        current = routes_traveled[0] #current is always front because is sorted
+        cost = calc_cost(loc, to_explore[0, :], final) + current.cost
+        routes_traveled[0].path.append(to_explore[0, :]) #MAKE SURE THIS WORKS!!
+        routes_traveled[0].cost = cost
+
+        to_explore = np.delete(to_explore, (0), axis=0) #removes first row
+
+        #THIS ADDS A NEW ROW
+        for possible_point in to_explore: #possible nodes to check
+
+            cost = calc_cost(loc, possible_point, final) + current.cost
+
+            new_route.cost = cost
+            new_route.path = current.path.append(possible_point)
+
+            routes_traveled.append(new_route)
 
 
+        routes_traveled = sorted(routes_traveled, key=lambda route: route.cost) #sorts based on lowest cost
 
+        loc = routes_traveled[0].path[-1] #DOUBLE CHECK THIS
+        print("changed road")
+        print("x:"+str(loc[0]) + " y:" +str(loc[1]))
+        if(loc[0] == final[0] and loc[1]== final[1]):
+            break
 
+    print(len(routes_traveled))
+    for route in routes_traveled:
+        print(route.path)
+        print(route.cost)
+
+roads = roads_array()
+loc = [10, 70]
+final = [80, 70]
+
+path_finder(roads, loc, final)
 
 print("success")
+
+
+
